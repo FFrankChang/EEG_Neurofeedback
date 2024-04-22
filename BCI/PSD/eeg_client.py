3e7dc9990c9aac5d9a47a9ae563e2a1dd59bdbe4
Frank_hmi_xiaomi2 <tjzzfrank@gmail.com>
2024-04-22 16:01:52 +0800

    add eeg_client (just receive and compute)

---
+++ BCI/PSD/eeg_client.py
@@ -0,0 +1,56 @@
+import numpy as np
+import mne
+from pylsl import StreamInlet, resolve_stream
+import xml.etree.ElementTree as ET
+import keyboard
+import socket
+
+def get_channel_names_from_info(info):
+    info_xml = info.as_xml()
+    root = ET.fromstring(info_xml)
+    channel_names = []
+    for channel in root.find('desc').find('channels').findall('channel'):
+        name = channel.find('label').text
+        channel_type = channel.find('type').text
+        if channel_type.upper() == 'EEG' and name in ['Fp1', 'Fpz', 'Fp2']:
+            channel_names.append(name)
+    return channel_names
+
+print("Looking for an EEG stream...")
+streams = resolve_stream('type', 'EEG')
+inlet = StreamInlet(streams[0])
+print("Stream found and connected.")
+
+channel_names = get_channel_names_from_info(inlet.info())
+sfreq = inlet.info().nominal_srate()
+sfreq = 1000  # Sampling frequency is 1000 Hz
+samples_per_epoch = 1000  # 1 second worth of data at 1000 Hz
+step_samples = 100  # Number of samples per step (0.1 seconds at 1000 Hz)
+buffer = np.zeros((samples_per_epoch, len(channel_names)))
+
+while True:
+    if keyboard.is_pressed('esc'):
+        print("Escape key pressed, exiting loop.")
+        break
+
+    sample, timestamp = inlet.pull_sample()
+    if sample:
+        buffer = np.roll(buffer, -step_samples, axis=0)  # Shift data to the left
+        buffer[-step_samples:] = sample[:-3]  # Insert new samples at the end
+
+        if len(buffer) >= samples_per_epoch:
+            data = buffer.T  # Transpose for MNE processing
+            ch_types = ['eeg'] * len(channel_names)
+            info = mne.create_info(ch_names=channel_names, sfreq=sfreq, ch_types=ch_types)
+            raw = mne.io.RawArray(data, info)
+            raw.filter(1, 40, fir_design='firwin')
+
+            spectrum = raw.compute_psd(method='welch', fmin=1, fmax=40, n_jobs=1)
+            freqs = spectrum.freqs
+            psd_data = spectrum.get_data()
+
+            bands = {'alpha': (8, 12), 'beta': (13, 30), 'theta': (4, 7), 'delta': (1, 3)}
+            power_bands = {band: np.mean(psd_data[:, np.logical_and(freqs >= freq_range[0], freqs <= freq_range[1])], axis=1) for band, freq_range in bands.items()}
+
+            message = f"{timestamp},{power_bands['alpha']},{power_bands['beta']},{power_bands['theta']},{power_bands['delta']}"
+            print(message)
