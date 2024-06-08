50b8b20da037af522f21c7c59a6102ef36ac24cf
shijian <571397889@qq.com>
2024-06-08 15:21:53 +0800

    update

--- data_analysis/script/wilcoxon_test.py
+++ data_analysis/script/wilcoxon_test.py
@@ -1,12 +1,20 @@
+import pandas as pd
 import numpy as np
 from scipy.stats import wilcoxon
 
-# 模拟数据
-data_with_feedback = np.random.normal(loc=0, scale=1, size=30)
-data_without_feedback = np.random.normal(loc=0.5, scale=1, size=30)
+# 加载数据
+data_path = '/mnt/data/output_file_hard.csv'
+data = pd.read_csv(data_path)
+
+# 分离每种条件的数据
+data_feedback = data[data['condition'] == 'feedback'].set_index('subject')['Steering_Angle_Std']
+data_silence = data[data['condition'] == 'silence'].set_index('subject')['Steering_Angle_Std']
+
+# 确保两个数据集按被试者对齐
+data_feedback = data_feedback.reindex(data_silence.index)
 
 # 执行Wilcoxon符号秩检验
-stat, p = wilcoxon(data_with_feedback, data_without_feedback)
+stat, p = wilcoxon(data_feedback, data_silence)
 
 # 输出结果
 print("Wilcoxon test statistic:", stat)
